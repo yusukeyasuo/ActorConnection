a1e72d3d1c832a4225f54389626bb33fe8097e32
Yusuke Yasuo <yasuo.y@dely.jp>
2021-03-23 20:19:39 +0900

    deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+set +e
+
+cd /home/ec2-user/ActorConnection/
+
+bundle install --without test development
+
+bundle exec rails assets:precompile RAILS_ENV=production
+
+bundle exec rails db:migrate RAILS_ENV=production
+
+cat tmp/pids/server.pid |xargs kill -9
+
+bundle exec rails s -e production
